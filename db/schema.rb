7198368778f6693e6528c5f76d3a9e4279bedce4
emacip <emacip@eaglealpha.com>
2015-08-11 14:24:57 +0100

    Change data base to Postgresql and schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150305133000) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookings", force: true do |t|
     t.integer  "room_id",          null: false
     t.date     "start_date",       null: false
