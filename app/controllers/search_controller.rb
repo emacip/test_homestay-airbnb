5d3cd3078f019f31dc8a57045abd55195c293397
emacip <emacipbelmonte@gmail.com>
2015-08-19 21:25:14 +0100

    changes for work with dates

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,18 @@
 class SearchController < ApplicationController
   def index
     @booking = Booking.new
-    if params[:booking].nil?
+
+    if params[:booking].nil? || params[:booking][:number_of_guests].blank? || params[:booking][:start_date].blank? || params[:booking][:end_date].blank?
       @bookings = []
     else
       #@rooms_available = Room.including_ids(Booking.all.map(&:room_id))
-      @bookings = (Booking.search filter:{range:{number_of_guests:{gt: params[:booking][:number_of_guests] }}}, size:2001, sort:{room_id:{order: "asc"}}).records.to_a
-      @bookings = Room.excluding_ids(@bookings.map(&:room_id)).paginate(:page => params[:page], :per_page => 20)
+      @unaveilable = (Booking.search filter:{
+                                      and:[{range:{number_of_guests:{gt: params[:booking][:number_of_guests] }}},
+                                           {term:{start_date: params[:booking][:start_date].to_date.strftime("%Y-%m-%d") }},
+                                           {term:{end_date: params[:booking][:end_date].to_date.strftime("%Y-%m-%d") }}
+                                          ]
+                                  }, size:2001, sort:{room_id:{order: "asc"}}).to_a
+      @bookings = Room.excluding_ids(@unaveilable.map(&:room_id)).paginate(:page => params[:page], :per_page => 20)
     end
   end
 end
-
