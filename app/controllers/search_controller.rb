724985ef9efbe12af6644eac4ec623113c627516
emacip <emacipbelmonte@gmail.com>
2015-08-20 19:46:37 +0100

    Last Updates in the search controller and in the views for check if the dates overlaps

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,11 +6,10 @@ class SearchController < ApplicationController
       @bookings = []
     else
       #@rooms_available = Room.including_ids(Booking.all.map(&:room_id))
-      @unaveilable = (Booking.search filter:{
-                                      and:[{range:{number_of_guests:{gt: params[:booking][:number_of_guests] }}},
-                                           {term:{start_date: params[:booking][:start_date].to_date.strftime("%Y-%m-%d") }},
-                                           {term:{end_date: params[:booking][:end_date].to_date.strftime("%Y-%m-%d") }}
-                                          ]
+      @unaveilable = (Booking.search filter:{and:[{range:{number_of_guests:{gt: params[:booking][:number_of_guests] }}},
+                                                  {range:{start_date:{gte: params[:booking][:start_date].to_date.strftime("%Y-%m-%d")}}},
+                                                  {range:{end_date:{lte: params[:booking][:end_date].to_date.strftime("%Y-%m-%d")}}}
+                                                ]
                                   }, size:2001, sort:{room_id:{order: "asc"}}).to_a
       @bookings = Room.excluding_ids(@unaveilable.map(&:room_id)).paginate(:page => params[:page], :per_page => 20)
     end
